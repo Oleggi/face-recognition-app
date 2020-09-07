8c2081f2a019e833a1d116d0c7a6d8e94ae61557
Oleg Borysko <49756993+Oleggi@users.noreply.github.com>
2020-09-07 15:17:54 +0300

    Updates

---
+++ precache-manifest.e2b514154dbfc1a37359db8d7163273b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3609174917ee11df4a2511d60f326e2",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "18d824ba5f0ac82c719a",
+    "url": "/face-recognition-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1ebf2a27d157562a71b9",
+    "url": "/face-recognition-app/static/css/main.1ae32587.chunk.css"
+  },
+  {
+    "revision": "18d824ba5f0ac82c719a",
+    "url": "/face-recognition-app/static/js/2.fd8211fc.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/face-recognition-app/static/js/2.fd8211fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ebf2a27d157562a71b9",
+    "url": "/face-recognition-app/static/js/main.e4237867.chunk.js"
+  },
+  {
+    "revision": "137b913ccf6bfc385e7c",
+    "url": "/face-recognition-app/static/js/runtime-main.b12322da.js"
+  },
+  {
+    "revision": "87466cd4f606eee3a29e9b9a78ef517a",
+    "url": "/face-recognition-app/static/media/brain-upper-view-outline.87466cd4.png"
+  }
+]);
